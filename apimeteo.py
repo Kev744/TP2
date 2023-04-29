e14c417ac7add0a2235e422cd47fa57eaf64f8af
Kev744 <94228138+Kev744@users.noreply.github.com>
2023-04-30 01:07:43 +0200

    Add files via upload

--- apimeteo.py
+++ apimeteo.py
@@ -6,14 +6,26 @@ Created on Fri Apr 28 11:52:48 2023
 """
 import json
 import requests 
-from flask import Flask, request
+from flask import Flask, request, render_template, flash, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kihg75'
 
 
 
-@app.route("/")
+
+@app.route("/", methods = ('GET','POST'))
 def home():
-    return json.loads('{ "test" : "Hello World!"}')
+    if request.method == 'POST':
+        lat = request.form['lat']
+        long = request.form['long']
+
+        if not lat:
+            flash('Fill out the latitude')
+        elif not long:
+            flash('Fill out the longitude')
+        else:
+            return redirect(f'/weather?lat={lat}&long={long}') 
+    return render_template("create.html")
 
 @app.route('/weather')
 def weather():
